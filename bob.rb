bdb6942ea7539d3999aaeaff67583fae749aa70a
Calin <calinoiu.alexandru@agilefreaks.com>
2013-08-31 16:17:31 +0300

    pass 3

--- bob.rb
+++ bob.rb
@@ -1,5 +1,10 @@
 class Bob
+	REPLIES = {
+		'WATCH OUT!' => 'Woah, chill out!',
+		'Does this cryogenic chamber make me look fat?' => 'Sure.'
+	}
+
 	def hey(message)
-		message == 'WATCH OUT!' ? 'Woah, chill out!': 'Whatever.'
+		REPLIES.has_key?(message) ? REPLIES[message] : 'Whatever.'
 	end
 end
